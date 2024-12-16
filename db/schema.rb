ae3be2fc4088b6de02d72d9abf443904d91323ae
SHG9-hub <zhuanzhitailang@gmail.com>
2024-12-17 03:58:22 +0900

    add_commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_16_162649) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_16_182921) do
+  create_table "attendances", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "check_in"
+    t.datetime "check_out"
+    t.decimal "work_hours"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_attendances_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -22,4 +32,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_16_162649) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "attendances", "users"
 end
