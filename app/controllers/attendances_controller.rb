9cc86f2cb4f72c6b38e8537acbe7cd1de055e326
SHG9-hub <zhuanzhitailang74@gmail.com>
2024-12-22 02:04:08 +0900

    add.update.commit

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,38 +1,39 @@
 class AttendancesController < ApplicationController
-    
-    def new
-        @user  = User.find(params[:user_id])                         # ユーザーの個人的なデータをとってくる。
-        @attendance =  Attendance.new
-    end
+  before_action :set_user
 
-    def create
-        @user = User.find(params[:user_id])
-        @attendance = @user.attendances.new(attendance_params)
-    
-        if @attendance.save
-          redirect_to user_path(@user), notice: "打刻が成功しました。"
-        else
-          render :new
-        end
-    end
+  # 出勤打刻
+  def create
+    @attendance = @user.attendances.new(attendance_params)
 
-    def update
-        @attendance = Attendance.find(params[:id])
-        
-        if @attendance.update(attendance_params)
-          redirect_to user_path(@attendance.user), notice: "退勤打刻が成功しました。"
-        else
-          render :edit
-        end
+    if @attendance.save
+      redirect_to user_path(@user), notice: "出勤打刻が成功しました。"
+    else
+      render :new
     end
-        
-    private
-    
-    def attendance_params
-        params.require(:attendance).permit(:check_in, :check_out)  # 必要なパラメータを許可
+  end
+
+  # 退勤打刻
+  def update
+    @attendance = @user.attendances.order(updated_at: :desc).first # 最新の勤怠を取得
+  
+    if @attendance.update(check_out: Time.current) # 退勤時刻を自動設定
+      flash[:notice] = "退勤が記録されました"
+    else
+      flash[:alert] = "退勤の記録に失敗しました"
     end
+    redirect_to user_path(@user)
+  end
+
+
+  private
+
+  # ユーザーを設定するメソッド
+  def set_user
+    @user = User.find(params[:user_id])  # URLからユーザーIDを取得
+  end
+
+  # アテンダンスのパラメータを許可するメソッド
+  def attendance_params
+    params.require(:attendance).permit(:check_in, :check_out)  # 出勤時間と退勤時間を許可
+  end
 end
-
-
-# 作るものとコントローラーはある程度一致する、という認識でOK。
-# 今回は「ユーザー」の「アテンダンス」の情報を作るので、アテンダンスコントローラーにクリエイトメソッドを記述。
