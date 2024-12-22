296be9be8e245a547630b3bca4bfaaa06f4f18fb
SHG9-hub <zhuanzhitailang74@gmail.com>
2024-12-23 01:26:56 +0900

    addupdate

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,7 +1,11 @@
 class AttendancesController < ApplicationController
   before_action :set_user
 
-  # 出勤打刻
+    # 出勤打刻
+  def new
+    @attendance = @user.attendances.new
+  end
+
   def create
     @attendance = @user.attendances.new(attendance_params)
 
@@ -13,17 +17,18 @@ class AttendancesController < ApplicationController
   end
 
   # 退勤打刻
-  def update
-    @attendance = @user.attendances.order(updated_at: :desc).first # 最新の勤怠を取得
-  
-    if @attendance.update(check_out: Time.current) # 退勤時刻を自動設定
-      flash[:notice] = "退勤が記録されました"
-    else
-      flash[:alert] = "退勤の記録に失敗しました"
-    end
-    redirect_to user_path(@user)
+  def edit
+    @attendance = @user.attendances.find_by(id: params[:id])
   end
 
+  def update
+    @attendance = @user.attendances.find_by(id: params[:id])
+    if @attendances.update(attendance_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
 
   # 下記構文使用必須。
   resources :users, only: %i[show index]  do
-    resources :attendances, only: %i[new create update] 
+    resources :attendances, only: %i[new create update edit ] 
   end
 
   # 今回ルートをこのようにネストさせた。
